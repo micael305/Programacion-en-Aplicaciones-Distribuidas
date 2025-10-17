04c22192ee2cc0d52ebd1e5c4f550d4d4fcc4ee3
micael305 <miicael305@gmail.com>
2025-10-17 10:05:51 -0300

    Web Service - Ejemplo

---
+++ Web Service - Ejemplo/WebApplication3/WebApplication3/WebService1.asmx.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Services;
+using WebApplication3.DataSet1TableAdapters;
+using static WebApplication3.DataSet1;
+
+namespace WebApplication3
+{
+    /// <summary>
+    /// Descripción breve de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    //https://localhost:44397/WebService1.asmx?wsdl
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+
+        [WebMethod]
+        public double pesosADolares(double montoEnPesos)
+        {
+            double montoEnDolares = montoEnPesos / 1500;
+            return montoEnDolares;
+        }
+        [WebMethod]
+        public PRECIOSDataTable getPrecios()
+        {
+            var adaptador = new PRECIOSTableAdapter();
+            //adapter.Insert(4, "nuevo", 1000);
+            DataSet1.PRECIOSDataTable tabla = adaptador.GetData(); //toda la info de las filas de PRECIO
+            return tabla;
+            ;
+        }
+
+
+        //public string getPrecios()
+        //{
+        //    var adaptador = new PRECIOSTableAdapter();
+        //    //adapter.Insert(4, "nuevo", 1000);
+        //    DataSet1.PRECIOSDataTable tabla = adaptador.GetData(); //toda la info de las filas de PRECIO
+        //    return tabla.Rows[0][2].ToString();
+
+    }
+}
---
+++ Web Service - Ejemplo/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var proxy = new ServiceReference1.WebService1SoapClient();
+            String respuesta = proxy.HelloWorld();
+            double importe = proxy.pesosADolares(Convert.ToDouble(textBox1.Text));
+            MessageBox.Show("Dolares: " + importe);
+        }
+    }
+}
+
+//engloc
+//Cliente
+//Gridview1.Datasource = proxy.getPrecios();
